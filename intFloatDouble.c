c5746637ab01c20ebf47db4aaec9a8518676df64
SPK-RPI <shibaprasadkar50@gmail.com>
2017-10-30 21:43:56 +0530

    commit intFloatDouble

---
+++ intFloatDouble.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+//#include<conio.h>
+void main(){
+    int num1;
+    float num2;
+    double num3;
+    num1=144;
+    num2=54.87;
+    num3=6.023;
+    printf("\n integer value=%d",num1);
+    printf("\n Float value=%f",num2);
+    printf("\nDouble value=%f",num3);
+    getchar();
+}
