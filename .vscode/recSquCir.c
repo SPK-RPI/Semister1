02affdaeef2d4a19b9f91474360918437afcfc15
SPK-RPI <shibaprasadkar50@gmail.com>
2017-10-30 22:30:59 +0530

    commet

---
+++ .vscode/recSquCir.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+#include<conio.h>
+void main(){
+float circle,r;
+int s,l,b,rect,squ;
+printf("\nEnter side:=");
+scanf("%d",&s);
+printf("\nEnter radius:==");
+scanf("%f",&r);
+
+    getchar();
+}
