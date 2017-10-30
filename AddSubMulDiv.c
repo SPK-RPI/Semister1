94a331958b352768cc3c9b15d2bd95b9c0a5c7de
SPK-RPI <shibaprasadkar50@gmail.com>
2017-10-30 21:51:52 +0530

    commit mul

---
+++ AddSubMulDiv.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+#include<conio.h>
+void main(){
+    int add,sub,mul,div;
+    int a=10,b=5;
+    add=a+b;
+    sub=a-b;
+    mul=a*b;
+    div=a/b;
+    printf("\nAddition =%d\nSubtraction=%d",add,sub);
+    printf("\nMultiplication=%d\nDivision=%d",mul,div);
+    getchar();
+
+}
