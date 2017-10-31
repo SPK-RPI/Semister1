58ce1b5b27cc4a49bad89567c9aaefedb1eed1e2
SPK-RPI <shibaprasadkar50@gmail.com>
2017-10-31 12:07:47 +0530

    commit

--- volCubSphCy.c
+++ volCubSphCy.c
@@ -1,22 +1,24 @@
 #include<stdio.h>
-#include<unistd.h>
+#include<curses.h>
+//#include <stdlib.h>
 //#include<conio.h>
-void main(){
+int main(){
 int s,r,h,cub;
 float cy,sp;
 printf("\nEnter side of the square:==");
 scanf("%d",&s);
 printf("\nEnter radius of the sphere:==");
-scanf("%f",&r);
+scanf("%d",&r);
 printf("\nEnter hight of the cylinder:==");
-scanf("%f",&h);
+scanf("%d",&h);
 sp=4/3*(3.14*r*r*r);
 cub=s*s*s;
 cy=3.14*r*r*h;
 printf("/nVolume of square:==%d",cub);
 printf("/nVolume of sphere:==%f",sp);
 printf("/nVolume of cylinder:==%f",cy);
+
 pause();
-    //getch();
-    return 0;
+return 0;
+ 
 }
