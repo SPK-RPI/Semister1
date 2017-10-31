d414689dc5bb5e17ad829abec8c66f67dba07bdf
SPK-RPI <shibaprasadkar50@gmail.com>
2017-10-31 10:42:14 +0530

    git

--- areaRecSquCir.c
+++ areaRecSquCir.c
@@ -1,20 +1,22 @@
+#include <unistd.h>
 #include<stdio.h>
 //#include<conio.h>
 void main(){
 float circle,r;
-int s,l,b,rect,squ;
+float s,l,b,rec,squ;
 printf("\nEnter side:==");
-scanf("%d",&s);
+scanf("%f",&s);
 printf("\nEnter radius:==");
 scanf("%f",&r);
-printf("\nEnter base and hight:");
-scanf("%d%d",&l,&b);
+printf("\nEnter base:");
+scanf("%f",&b);
+printf("\nEnter hight:");
+scanf("%f",&l);
 circle=3.14*r*r;
 squ=s*s;
-rect=l*b;
+rec=b*l;
 printf("\nArea of circle =%f",circle);
 printf("\nArea of square =%f",squ);
-printf("\nArea of rectangle=%f",rect);
-
-getchar();
+printf("\nArea of rectangle =%f",rec);
+pause();
 }
