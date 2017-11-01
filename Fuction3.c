fd6119b4218e49d0d3af76a5ff96fb3826b2963f
SPK-RPI <shibaprasadkar50@gmail.com>
2017-11-01 20:38:20 +0530

    git

---
+++ Fuction3.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+//#include<conio.h>
+int main(){
+display(2,3);
+while(getchar()!="\n");
+}
+void display(int a,int b){
+    int c;
+    c=a+b;
+    printf("Type fUCTION WITH ARGUMENTS AND NO RETURN VALUE");
+    printf("\n\nAddition is =%d",c);
+}
