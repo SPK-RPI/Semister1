bfb21c24eb078943e13e7f8054179c0931c551ba
SPK-RPI <shibaprasadkar50@gmail.com>
2017-11-01 20:06:09 +0530

    git

---
+++ pattern4.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+//#include<conio.h>
+void main(){
+    int r,c,s;
+    //clrscr();
+    for(r=1;r<=5;r++){
+        for(s=1;s<=r;s--){
+            printf(" ");
+        }
+        for(c=1;c<=r;c++){
+        printf("*");
+        }
+        printf("\n");
+    }
+    while(getchar()!="\n");
+}
