e97dce2142a52651feff31df22493f3514c643a0
SPK-RPI <shibaprasadkar50@gmail.com>
2017-11-05 22:37:59 +0530

    class

--- class/test.c
+++ class/test.c
@@ -1,10 +1,13 @@
-#include <ncurses.h>
-int main(){
-    initscr();
-    printw("Hai thar world...");
-    refresh();
-    getch();
-    endwin();
-//while(getchar()!="\n");
-    return 0;
+#include<stdio.h>
+#include<math.h>
+int main()
+{
+float p,i;
+double f;
+int n;
+printf("\nenter values for i, p and n" );
+scanf ("%f%f%d",&i,&p,&n);
+f = p* pow( 1+i,n);
+printf("\nthe out is %lf",f);
+while(getchar()!="\n");
 }
