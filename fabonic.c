234d18b8d2e03e29cf5f527f4df3c6a35c239693
SPK-RPI <shibaprasadkar50@gmail.com>
2017-11-10 00:00:25 +0530

    git

--- fabonic.c
+++ fabonic.c
@@ -1,13 +1,13 @@
 #include<stdio.h>
 //#include<conio.h>
-void main(){
+int main(){
 int n,t1=0,t2=1,nextterm,i;
 printf("\nEnter a number.: ");
 scanf("\n%d",&n);
 for(i=1;i<=n;i++){
 printf("\t%d",t1);
 nextterm=t1+t2;
-t1+t2;
+t1=t2;
 t2=nextterm;
 }
 while(getchar()!="/n");
